f3131b820e294560e68d9efcb8fe435429bbcb57
Md. Nazmul Huda <me@nazmul-huda.com>
2025-07-02 14:25:14 +0600

    optimized deployment

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,35 @@
-import express, { Request, Response } from 'express'
+// LOCAL IMPORTS
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import express, { NextFunction, Request, Response } from 'express'
 
+// VARIABLES
 const port = process.env.PORT || 3000
 const app = express()
+
+// LOGGER
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  console.log(`Request made to: ${req.url}`)
+  next()
+})
+
+// STATIC ROUTE
+const thisDirectory = path.dirname(fileURLToPath(import.meta.url))
+const publicFolder = path.resolve(thisDirectory, '..', 'public')
+
+app.use(express.static(publicFolder))
+
+// API ROUTE
+app.use(express.json()) // accepts all body as JSON
 
 app.all('/api', function (req: Request, res: Response) {
   res.json({ message: `API route is working!!!` })
 })
 
+// RUN SERVER
 app.listen(port, function () {
   console.log(`Server is running on PORT: ${port}`)
 })
+
+// EXPORT FOR VERCEL
+export default app
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>Building frontend!</>
+}
+
+export default App
